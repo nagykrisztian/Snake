1ba1408aac85e483343563fe7bd4de80c988fe02
nagykrisztian <krisznagy2001@gmail.com>
2021-02-25 10:00:11 +0100

    Az elfordulások megvalósítása

--- snake.py
+++ snake.py
@@ -1,11 +1,23 @@
 import turtle
 import time
 
+
+def jobbra():
+  fej.right(90)
+
+
+def balra():
+  fej.left(90)
+
+
 palya = turtle.Screen()
 palya.setup(width=800, height=600)
 palya.bgcolor("green")
 palya.title("Snake")
 palya.tracer(0)
+palya.listen()
+palya.onkey(jobbra, "Right")
+palya.onkey(balra, "Left")
 
 fej = turtle.Turtle()
 fej.shape("triangle")
@@ -16,4 +28,3 @@ while True:
   fej.forward(20)
   palya.update()
   time.sleep(0.3)
-
